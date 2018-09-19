ea190e4c406c755b6280e8dc0bc650d960b8f390
Joshua Chinyama <chinyamajosh@gmail.com>
2018-09-19 11:15:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your functions and code here
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+  ];
+  
+  function destructivelyAppendKitten(name) {
+    kittens.push('Ralph');
+    return kittens;
+  }
+  
+  function destructivelyPrependKitten(name) {
+    kittens.unshift('Bob');
+    return kittens;
+  }
+  
+ function destructivelyRemoveLastKitten(name) {
+   kittens.pop();
+   return kittens;
+ }
+  
+  function destructivelyRemoveFirstKitten(name) {
+    kittens.shift();
+    return kittens;
+  }
+  
+  function appendKitten(name) { 
+    
+
+  
+  
